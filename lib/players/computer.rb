c9ed98ff6133f2ce5c53b320da243cbbc9395d73
taisaoko <ttd3calvin@gmail.com>
2018-10-21 21:18:14 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -93,3 +93,12 @@ module Players
     end
   end
 end
+
+# - AI Logic -
+# 1. Check to see if the computer has a winning combo with two tokens and one empty space
+# --> If yes: put the token in the empty space
+# 2. Check to see if the opponent has a winning combo with two tokens and one empty space
+# --> If yes: put the token in the empty space
+# 3. Else, check if the middle space is open
+# 4. Else, check if a corner space is open
+# 5. Else, choose another space at random
